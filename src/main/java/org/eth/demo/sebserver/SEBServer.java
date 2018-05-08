5bfd175b036229fc2612729444c5288edb20183d
anhefti <andreas.hefti@let.ethz.ch>
2018-05-08 14:47:53 +0200

    added copyright

--- src/main/java/org/eth/demo/sebserver/SEBServer.java
+++ src/main/java/org/eth/demo/sebserver/SEBServer.java
@@ -1,12 +1,18 @@
+/*
+ * Copyright (c) 2018 ETH Zürich, Educational Development and Technology (LET)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package org.eth.demo.sebserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SEBServer {
-
 
     public static void main(final String[] args) {
         SpringApplication.run(SEBServer.class, args);
