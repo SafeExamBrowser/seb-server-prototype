a05fec067de860c2edd9ad69584a8f7637ef6eba
anhefti <andreas.hefti@let.ethz.ch>
2018-07-06 17:08:21 +0200

    added basic security

---
+++ src/main/java/org/eth/demo/sebserver/web/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2018 ETH Zürich, Educational Development and Technology (LET)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.eth.demo.sebserver.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/rwt-resources/*", "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .logout()
+                .permitAll()
+                .and().csrf().disable() // TODO enable for RAP gui?
+        ;
+    }
+}
